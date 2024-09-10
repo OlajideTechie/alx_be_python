d866876ecfdca7e6a1575907e3b4431f549eced8
Olajide Ojo <oolajide91@gmail.com>
2024-09-10 19:14:55 +0100

    basic arithmetic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#define two variables 
+number1 = 10
+number2 = 5
+
+#perform operations
+result_sum = number1 + number2
+result_diff = number1 - number2
+result_product = number1 * number2
+
+#print the resukt
+print(f"Addition of {number1} and {number2} is {result_sum}")
+print(f"Subtraction of {number1} and {number2} is {result_diff}")
+print(f"Multiplication of {number1} and {number2} is {result_product}")
+
+
