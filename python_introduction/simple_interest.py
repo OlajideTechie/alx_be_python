cbf99fcbb66d0c271068fa6d6336bcf6bf250522
Olajide Ojo <oolajide91@gmail.com>
2024-09-10 19:35:28 +0100

    simple intrest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#simple intrest calculator
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate the interest
+interest = principal * rate * time; 
+
+#print the result 
+print(f"The simple interest is: {interest}")
