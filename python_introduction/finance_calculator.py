292ec6d166cc8002b0a5fbb85f87a88bd455dca7
Olajide Ojo <oolajide91@gmail.com>
2024-09-11 07:18:30 +0100

    finance calculator script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,17 @@
+
+income_input = input('Enter your monthly income:')
+monthly_income = int(income_input)
+
+income_input = input('Enter your monthly expenses:')
+monthly_expenses = int(income_input)
+
+#calculate monthky savings 
+monthly_savings = monthly_income - monthly_expenses
+
+#Calculate the projected savings for a year 
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+#Print the Result
+print(f"your monthly savings are ${monthly_savings:.2f}. Projected savings after one year, with interest, is: ${projected_savings:.2f}")
+
+
--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -10,3 +10,4 @@ current_year = 2023
 future_age =  current_age + (future_year - current_year);
 
 print(f"In 2050, you will be {future_age} years old.")
+
