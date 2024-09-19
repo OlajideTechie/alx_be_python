d7c7837ba3c93d0e70d7746455a380dcf6086a52
Olajide Ojo <oolajide91@gmail.com>
2024-09-19 15:38:38 +0100

    another code fix

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -5,4 +5,4 @@ for numbers in range(1, 11): #loop to iterate the number times to display
         
         product = number * numbers
         
-        print(f"{number} x {numbers} = {product}", end="\t")
+        print(f"{number} x {numbers} = {product}")
