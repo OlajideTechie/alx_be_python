902a5ad4a42fa49965d08ff36194f291d098c8fb
Olajide Ojo <oolajide91@gmail.com>
2024-09-19 08:23:16 +0100

    assign waather as a variable for user input

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
-#Prompt the user to select a weather of the day. 
-prompt = input("What's the weather like today? (sunny/rainy/cold):  ").strip().lower()  
+# Prompt the user to select the weather for the day.
+weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
 # Provide clothing recommendations based on the weather.
-if prompt == 'sunny': 
+if weather == 'sunny':
     print('Wear a t-shirt and sunglasses.')
-elif prompt == 'rainy':
-      print("Don't forget your umbrella and a raincoat.")
-elif prompt == 'cold':
-        print(' Make sure to wear a warm coat and a scarf')
+elif weather == 'rainy':
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == 'cold':
+    print('Make sure to wear a warm coat and a scarf.')
 else:
-        print("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
