7c9430b6b697f9376d21635c56d619b71311517b
Olajide Ojo <oolajide91@gmail.com>
2024-09-19 15:52:23 +0100

    beans don done

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input('Enter a number to see its multiplication table:'))  # Take u
 
 for multiplier in range(1, 11):  # Loop to iterate the number from 1 to 10.
     product = number * multiplier  
-    print(f"{number} x {multiplier} = {product}")
+    print(f"{number} * {multiplier} = {product}")
