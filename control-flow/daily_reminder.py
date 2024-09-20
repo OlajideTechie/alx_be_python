960f9b8ccf16767f8b2ddb1c122de4170569b1eb
Olajide Ojo <oolajide91@gmail.com>
2024-09-20 12:54:12 +0100

    add correct case name

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,13 +3,13 @@ priority = input('Priority (high/medium/low): ').strip()
 time_bound = input('Is it time-bound? (yes/no): ').strip()
 
 match priority:
-    case '_':
+    case 'high':
         if priority == 'high' and time_bound == 'yes':
             print(f'{task} is a high priority task ')
-    case '_':
+    case 'medium':
         if priority == 'medium' and time_bound == 'no':
             print(f'{task} is a medium priority task that can be done at a later time today.')
-    case '_':
+    case 'low':
         if priority == 'low' and time_bound == 'no':
             print(f'{task} is a low priority task consider completing it when you have free time.')
 if time_bound == 'yes':
