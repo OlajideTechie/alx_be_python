163e770cd4ca423055820dfd76bffbf5d742f713
Olajide Ojo <oolajide91@gmail.com>
2024-09-20 05:57:45 +0100

    remove .strip() function

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 # Prompt user to enter two numbers and select an operation (addition, subtraction, multiplication, or division).
-num1 = int(input('Enter the first number:')).strip()
-num2 = int(input('Enter the second number:')).strip()
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
 
 operation = input('Choose the operation (+, -, *, /):').strip()
 
