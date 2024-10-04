aa1e7212afb254a50ab21ecb55c8b41c715a10ea
Olajide Ojo <oolajide91@gmail.com>
2024-10-04 07:36:40 +0100

    refactor 1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,29 +10,34 @@ class BankAccount:
         """Deposit money into the account."""
         try:
             amount = float(amount)  # Ensure amount is a float
-            if amount > 0:
-                self.account_balance += amount
-                return f"Deposited ${amount:.2f}"
-            else:
-                return 'Invalid deposit amount. Please enter a positive number.'
+            if amount <= 0:
+                   return 'Invalid deposit amount. Please enter a positive number.'
+                       
+            self.account_balance += amount
+            return f"Deposited ${amount:.2f}"
+        
         except ValueError:
             return 'Invalid input. Please enter a numeric value.'
+        
 
     def withdraw(self, amount):
         """Withdraw money from the account."""
         try:
             amount = float(amount)  # Ensure amount is a float
-            if amount > 0:
-                if amount <= self.account_balance:
-                    self.account_balance -= amount
-                    return f'Withdrew ${amount:.2f}'  # Indicate success
-                else:
-                     return f'Insufficient funds.'   # Insufficient funds
-            else:
-                return 'Invalid withdrawal amount. Please enter a positive number.'
+            if amount <= 0:
+                 return 'Invalid withdrawal amount. Please enter a positive number.'
+                    
+            if amount > self.account_balance:
+                return 'Insufficient funds.'
+            
+            self.account_balance -= amount
+            return f'Withdrew ${amount:.2f}'
+        
         except ValueError:
-            return 'Invalid input. Please enter a numeric value.'
+               return 'Invalid input. Please enter a numeric value.'
+           
 
     def display_balance(self):
         """Display the current account balance."""
         return f"Current Balance: ${self.account_balance:.2f}"
+
