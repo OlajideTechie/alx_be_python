b83b845d24d58863565f85750c2c91d69f85d228
Olajide Ojo <oolajide91@gmail.com>
2024-10-06 22:42:24 +0100

    refactor withdrwal logic

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,7 @@ class BankAccount:
     
     def __init__(self, initial_balance=0):
         """Initialize the bank account with a non-negative balance."""
-        self._account_balance = float(initial_balance) if float(initial_balance) >= 0 else 0
+        self.account_balance = float(initial_balance) if float(initial_balance) >= 0 else 0
 
     def deposit(self, amount):
         """Deposit money into the account."""
@@ -13,7 +13,7 @@ class BankAccount:
             if amount <= 0:
                    return 'Invalid deposit amount. Please enter a positive number.'
                        
-            self._account_balance += amount
+            self.account_balance += amount
             return f"Deposited ${amount}"
         
         except ValueError:
@@ -23,21 +23,17 @@ class BankAccount:
     def withdraw(self, amount):
         """Withdraw money from the account."""
         try:
-            amount = float(amount)  # Ensure amount is a float
-            if amount <= 0:
-                 return 'Invalid withdrawal amount. Please enter a positive number.'
-                    
-            if amount > self._account_balance:
-                return 'Insufficient funds.'
-            
-            self._account_balance -= amount
-            return f'Withdrew ${amount}'
-        
+             amount = float(amount)  # Ensure amount is a float
+             if self.account_balance > 0 and self.account_balance >= amount :
+               self.account_balance -= amount
+               return True
+             else:
+               return False            
         except ValueError:
                return 'Invalid input. Please enter a numeric value.'
            
 
     def display_balance(self):
         """Display the current account balance."""
-        return f"Current Balance: ${self._account_balance:.2f}"
+        return f"Current Balance: ${self.account_balance: .2f}"
 
